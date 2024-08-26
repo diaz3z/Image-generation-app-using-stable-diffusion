46d8e2e8d14c42f809da8b46296fd7f9cb7d9ca0
Mohammad Zaid <mdzaid786291@gmail.com>
2024-08-27 00:17:13 +0530

    Final commit

--- styling.py
+++ styling.py
@@ -1,10 +1,9 @@
 import tkinter as tk
 import customtkinter as ctk
 from torch.cuda.amp import autocast  # Updated import
-from PIL import Image, ImageTk
-import torch
-from diffusers import StableDiffusionPipeline
 from tkinter import filedialog
+
+
 
 app = tk.Tk()
 app.geometry("1200x720")
@@ -29,45 +28,6 @@ trigger.place(x=920, y=55)
 save_button = ctk.CTkButton(master=app, height=30, width=120, corner_radius=7, font=("Times New Roman Baltic", 20), text_color="black", fg_color="skyblue", text="Save", command=lambda: save_images())
 save_button.place(x=540, y=670)
 
-model_id = "CompVis/stable-diffusion-v1-4"
-device = "cuda"
-pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
-pipe = pipe.to(device)
 
-generated_images = []
-
-
-def generate():
-    promt_text = promt.get("1.0", "end-1c")
-    with autocast():
-        image = pipe(promt_text, num_images_per_prompt=2,guidance_scale=9.5).images
-
-    global generated_images
-    generated_images = image
-
-    img1 = ImageTk.PhotoImage(image[0])
-    lmain1.configure(image=img1)
-    lmain1.image = img1
-
-    img2 = ImageTk.PhotoImage(image[1])
-    lmain2.configure(image=img2)
-    lmain2.image = img2
-
-
-def save_images():
-    if not generated_images:
-        print("No images to save")
-        return
-
-    # Save the first image
-    file_path_1 = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG files", "*.png")])
-    if file_path_1:
-        generated_images[0].save(file_path_1)
-
-    # Save the second image
-    file_path_2 = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG files", "*.png")])
-    if file_path_2:
-        generated_images[1].save(file_path_2)
 
 app.mainloop()
-
